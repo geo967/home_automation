8b1052a7a5623f740bf64a8d931efa12909bcae2
geo967 <geomathew967@gmail.com>
2025-12-13 09:14:18 +0530

    - setup home screen adapter and activity

--- app/src/main/java/com/cblue/home_automation/HomeScreen.kt
+++ app/src/main/java/com/cblue/home_automation/HomeScreen.kt
@@ -1,13 +1,94 @@
 package com.cblue.home_automation
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
-class HomeScreen : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val toggleButton: Button by bind(R.id.toggle)
+    private val statusTextView: TextView by bind(R.id.status)
+    private val recyclerView: RecyclerView by bind(R.id.recycler_view)
+
+    private val nsdManagerRx: NsdManagerFlow by lazy { NsdManagerFlow(this) }
+    private val adapter: DiscoveryAdapter by lazy { DiscoveryAdapter() }
+    private var jobState = MutableStateFlow<Job?>(null)
+
+    private fun <T : View> Activity.bind(@IdRes id: Int) = lazy { findViewById<T>(id) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_screen)
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        recyclerView.adapter = adapter
+
+        toggleButton.setOnClickListener { toggle() }
+        updateUI()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopDiscovery()
+    }
+
+    private fun updateUI() {
+        statusTextView.setText(
+            if (jobState.value == null) R.string.activity_main_status_discovery_off
+            else R.string.activity_main_status_discovery_on
+        )
+    }
+
+    private fun toggle() {
+        if (jobState.value == null) startDiscovery()
+        else stopDiscovery()
+    }
+
+    private fun startDiscovery() {
+        jobState.value = MainScope().launch {
+            nsdManagerRx.discoverServices(DiscoveryConfiguration("_services._dns-sd._udp"))
+                .catch { e -> Log.e(TAG, "Error starting discovery.", e) }
+                .collect(::addServiceFromEvent)
+        }
+        updateUI()
+    }
+
+    private fun addServiceFromEvent(event: DiscoveryEvent) {
+        Log.d(TAG, "Event $event")
+        when (event) {
+            is DiscoveryServiceFound -> adapter.addItem(event.service.toDiscoveryRecord())
+            is DiscoveryServiceLost -> adapter.removeItem(event.service.toDiscoveryRecord())
+            else -> {}
+        }
+    }
+
+    private fun stopDiscovery() {
+        jobState.value?.cancel()
+        jobState.value = null
+        updateUI()
+        adapter.clear()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
